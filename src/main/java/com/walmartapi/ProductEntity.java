5ffd67b22b8e22ee88717c1e05b668f0bfc5447b
nicolasG27 <ngm12199@gmail.com>
2026-03-25 17:53:12 -0700

    Se estableció la tabla q hace mapeo con base de datos en clase ProductEntity

---
+++ src/main/java/com/walmartapi/ProductEntity.java
@@ -0,0 +1,17 @@
+package com.walmartapi;
+
+
+import jakarta.persistence.*;
+
+@Entity //hace un mapeo de un objeto con una tabla de base de datos
+@Table(name = "products")
+public class ProductEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+}
+
+//Cuando haces git clone el remoto lo asigna a donde lo clonaste
